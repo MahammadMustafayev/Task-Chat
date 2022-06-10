a49d7b3ec3345fec6f9f4e40dea827c4da67f44a
MahammadMustafayev <99835006+MahammadMustafayev@users.noreply.github.com>
2022-06-10 21:41:14 +0300

    added

---
+++ AzTUChat-master/AzTUChat/Controllers/AccountController.cs
@@ -0,0 +1,77 @@
+﻿using AzTUChat.DAL;
+using AzTUChat.Models;
+using AzTUChat.ViewModel;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzTUChat.Controllers
+{
+    public class AccountController : Controller
+
+    {
+        private UserManager<AppUser> _userManager { get;  }
+        private IWebHostEnvironment _env { get;  }
+        private AppDbContext _context { get;  }
+        private SignInManager<AppUser> _signInManager { get;  }
+        public AccountController(UserManager<AppUser> userManager, 
+                                 IWebHostEnvironment env,
+                                 AppDbContext context,
+                                 SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+            _context = context;
+            _env = env;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(LoginVM login)
+        {
+            AppUser user = _context.Users.SingleOrDefault(u => u.UserName == login.UserName);
+           var result= await _signInManager.PasswordSignInAsync(user, login.Password,true,false);
+            if (!result.Succeeded)
+            {
+                return View(login);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterVM register)
+        {
+            AppUser user = new AppUser
+            {
+                FullName = register.FullName,
+                UserName = register.UserName
+            };
+            await _userManager.CreateAsync(user, register.Password);
+            string fileName = register.UserName + register.Image.FileName;
+            using(FileStream fs = new FileStream(Path.Combine(_env.WebRootPath,"img",fileName),FileMode.Create))
+            {
+                register.Image.CopyTo(fs);
+            }
+            UserImage ui = new UserImage
+            {
+                AppUser = user,
+                ImageUrl= fileName
+            };
+            await _context.UserImages.AddAsync(ui);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
---
+++ AzTUChat-master/AzTUChat/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using AzTUChat.DAL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzTUChat.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private AppDbContext _context { get; }
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Users.Include(u=>u.Image));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
